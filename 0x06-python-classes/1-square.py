466b353428e44e98f57648a573b5610d7c475ecd
Marilynn-lynn <Marilyn-lynn@gmail.com>
2023-09-27 09:02:02 -0700

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    """Print an integer with "{:d}".format().
-    Args:
-        value (int): The integer to print.
-    Returns:
-        If a TypeError or ValueError occurs - False.
-        Otherwise - True.
-    """
-    try:
-        print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
-        return (False)
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
